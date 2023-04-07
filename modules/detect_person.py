c8d5dd1b37109411a28af47b912b4f3a1b294ec6
gnsdp99 <rladpgns99@gmail.com>
2023-04-08 00:44:51 +0900

    Update detect function to Class

--- modules/detect_person.py
+++ modules/detect_person.py
@@ -1,115 +1,98 @@
-import argparse
-import time
 from pathlib import Path
 import os
 
-import cv2
 import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.datasets import LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, set_logging
+from utils.torch_utils import select_device, load_classifier
 
-
-def detect(source, save_img=False):
-    weights, imgsz, conf_thres, iou_thres, device, classes = 'yolov7.pt', 640, 0.25, 0.45, '', None
-    view_img = save_txt = save_conf = augment = agnostic_nms = update = exist_ok = False
-    trace = True
-    
-    # Initialize
-    set_logging()
-    device = select_device(device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
-
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
-
-    if half:
-        model.half()  # to FP16
-
-    # Second-stage classifier
-    classify = False
-    if classify:
-        modelc = load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
-
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    old_img_w = old_img_h = imgsz
-    old_img_b = 1
-
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
-
-        # Warmup
-        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
-            old_img_b = img.shape[0]
-            old_img_h = img.shape[2]
-            old_img_w = img.shape[3]
-            for i in range(3):
-                model(img, augment=augment)[0]
-
-        # Inference
-        t1 = time_synchronized()
-        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-            pred = model(img, augment=augment)[0]
-        t2 = time_synchronized()
-
-        # Apply NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
-        t3 = time_synchronized()
-
-        # Apply Classifier
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
-
-        # Process detections
-        has_person = 0 
-        for i, det in enumerate(pred):  # detections per image
-            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
-
-            p = Path(p)  # to Path
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-                    if not has_person and names[int(c)] == 'person':
-                        has_person = 1
-            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+class Detector():
+    def __init__(self):
+        self.weights = 'yolov7.pt'  # 'model.pt path(s)'
+        self.imgsz = 320  # 'inference size (pixels)'
+        self.conf_thres = 0.25  # 'object confidence threshold
+        self.iou_thres = 0.45  # 'IOU threshold for NMS'
+        self.classes = None  # 'filter by class: --class 0, or --class 0 2 3'
+        self.augment = False  # 'augmented inference'
+        self.agnostic_nms = False  # 'class-agnostic NMS'
+        set_logging()
+        self.device = select_device('')  # 'cuda device, i.e. 0 or 0,1,2,3 or cpu'
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
         
-        # remove the image if it has no any person.
-        if not has_person:
-            try:
-                os.remove(path)
-                print("the image is removed.")
-            except FileNotFoundError as e:
-                print(e)
+        # Load model
+        self.model = attempt_load(self.weights, map_location=self.device)  # load FP32 model
+        self.stride = int(self.model.stride.max())  # model stride
+        self.imgsz = check_img_size(self.imgsz, s=self.stride)  # check img_size
+        
+        if self.half:
+            self.model.half()  # to FP16
+            
+        # Second-stage classifier
+        self.classify = False
+        if self.classify:
+            self.modelc = load_classifier(name='resnet101', n=2)  # initialize
+            self.modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=self.device)['model']).to(self.device).eval()
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
+        # Get names and colors
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, self.imgsz, self.imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
+        self.old_img_w = self.old_img_h = self.imgsz
+        self.old_img_b = 1
+        
+    def detect(self, source):
+        # Set Dataloader
+        self.dataset = LoadImages(source, img_size=self.imgsz, stride=self.stride)
+
+        # Run inference
+        for path, img, im0s, _ in self.dataset:
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if self.half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            # Warmup
+            if self.device.type != 'cpu' and (self.old_img_b != img.shape[0] or self.old_img_h != img.shape[2] or self.old_img_w != img.shape[3]):
+                self.old_img_b = img.shape[0]
+                self.old_img_h = img.shape[2]
+                self.old_img_w = img.shape[3]
+                for _ in range(3):
+                    self.model(img, augment=self.augment)[0]
+
+            # Inference
+            with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+                self.pred = self.model(img, augment=self.augment)[0]
+
+            # Apply NMS
+            self.pred = non_max_suppression(self.pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
+
+            # Apply Classifier
+            if self.classify:
+                self.pred = apply_classifier(self.pred, self.modelc, img, im0s)
+
+            # Process detections
+            has_person = 0 
+            for det in self.pred:  # detections per image
+                p, im0 = path, im0s
+
+                p = Path(p)  # to Path
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+ 
+                    # Print results
+                    for c in det[:, -1].unique():
+                        if not has_person and self.names[int(c)] == 'person':
+                            has_person = 1
+            
+            # remove the image if it has no any person.
+            if not has_person:
+                try:
+                    os.remove(path)
+                    print("the image is removed.")
+                except FileNotFoundError as e:
+                    print(e)
