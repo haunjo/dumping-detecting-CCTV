a2b6dcdc817e780d72d0351987d890b81c8c5528
HaunJo <johaun12@naver.com>
2023-04-12 23:28:18 +0900

    api 완성

--- api.py
+++ api.py
@@ -1,14 +1,25 @@
 import main
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+import cv2
 
 app = FastAPI()
 
 @app.post("/files/")
 async def create_file(file : UploadFile):
     content = await file.read()
-    
-        
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    with torch.no_grad():
+        det = human_detector.detect(img)
+    for i, (*xyxy, conf, cls) in enumerate(reversed(det)):
+        # xyxy -> (left_bottom_x, left_bottom_y, right_top_x, right_top_y)
+        if cls == 0: # only save an image of person
+            crropedImage = crop_image(img_path, tuple(map(float, xyxy)))
+            pred = dumping_classifier.classify(crropedImage)
+            print(pred)
+            if pred == "Dumping":
+                print(img_path)
     return JSONResponse({"filename" : file.filename})
 
 @app.get("/")
