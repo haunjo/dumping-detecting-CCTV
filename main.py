3a6bd172e926fbc5a380a37d4b0385a6b3a90cf8
HaunJo <johaun12@naver.com>
2023-04-12 19:15:39 +0900

    우분투 환경설정 테스트

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data import ImageFromVideo
 from data import ImageFromCam
-from fastapi import FastAPI
+#from fastapi import FastAPI
 
 from modules.detect_person import detect
 from modules.classify import classify
@@ -10,7 +10,7 @@ import torch
 #ImageFromVideo.get_image('data/video/2023032319.mov')
 
 if __name__ == "__main__":
-    for img_path in ImageFromCam.get_image(): # get image path each second.
+    for img_path in ImageFromVideo.get_image('data/images/IMG_1031.MOV'): # get image path each second.
         with torch.no_grad():
             detect(img_path)
         try:
