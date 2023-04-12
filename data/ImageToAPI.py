4ef5bdf2d47f200ec6f8a556d03c30643eb0936c
HaunJo <johaun12@naver.com>
2023-04-12 22:53:38 +0900

    fast api inital 코드 완성

---
+++ data/ImageToAPI.py
@@ -0,0 +1,17 @@
+import ImageFromCam
+import ImageFromVideo
+import requests
+
+url =  "http://localhost:8000/files/"
+filename = "images/2023_04_12_191946.jpg"
+
+with open(filename, "rb") as f:
+    contents = f.read()
+
+files = {"file" : (filename, contents, "image/jpg")}
+
+response = requests.post(url, files = files)
+print(response.json())
+
+
+
