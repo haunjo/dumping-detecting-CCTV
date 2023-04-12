1d9ce592656bd3a45e329c45b7b63589eae07cb8
gnsdp99 <rladpgns99@gmail.com>
2023-04-13 00:09:07 +0900

    Update detect.py type of source str -> np.ndarray

--- modules/detect.py
+++ modules/detect.py
@@ -1,7 +1,8 @@
 import torch
+import numpy as np
 
 from models.experimental import attempt_load
-from utils.datasets import LoadImages
+from utils.datasets import LoadImages_fromImage
 from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, set_logging
 from utils.torch_utils import select_device, load_classifier
 
@@ -40,43 +41,42 @@ class Detector():
         self.old_img_w = self.old_img_h = self.imgsz
         self.old_img_b = 1
         
-    def detect(self, source: str) -> torch.Tensor:
+    def detect(self, source: np.ndarray) -> torch.Tensor:
         # Set Dataloader
-        dataset = LoadImages(source, img_size=self.imgsz, stride=self.stride)
-
+        img, im0s = LoadImages_fromImage(source, img_size=self.imgsz, stride=self.stride)
+        
         # Run inference
-        for path, img, im0s, _ in dataset:
-            img = torch.from_numpy(img).to(self.device)
-            img = img.half() if self.half else img.float()  # uint8 to fp16/32
-            img /= 255.0  # 0 - 255 to 0.0 - 1.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-            # Warmup
-            if self.device.type != 'cpu' and (self.old_img_b != img.shape[0] or self.old_img_h != img.shape[2] or self.old_img_w != img.shape[3]):
-                self.old_img_b = img.shape[0]
-                self.old_img_h = img.shape[2]
-                self.old_img_w = img.shape[3]
-                for _ in range(3):
-                    self.model(img, augment=self.augment)[0]
+        # Warmup
+        if self.device.type != 'cpu' and (self.old_img_b != img.shape[0] or self.old_img_h != img.shape[2] or self.old_img_w != img.shape[3]):
+            self.old_img_b = img.shape[0]
+            self.old_img_h = img.shape[2]
+            self.old_img_w = img.shape[3]
+            for _ in range(3):
+                self.model(img, augment=self.augment)[0]
 
-            # Inference
-            with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-                pred = self.model(img, augment=self.augment)[0]
+        # Inference
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = self.model(img, augment=self.augment)[0]
 
-            # Apply NMS
-            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
+        # Apply NMS
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
 
-            # Apply Classifier
-            if self.classify:
-                pred = apply_classifier(self.pred, self.modelc, img, im0s)
+        # Apply Classifier
+        if self.classify:
+            pred = apply_classifier(self.pred, self.modelc, img, im0s)
 
-            # Process detections
-            for det in pred:  # detections per image
-                im0 = im0s
-                if len(det):
-                    # Rescale boxes from img_size to im0 size
-                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-                  
-            # return the results of detection (*xyxy, conf, cls)
-            return det
+        # Process detections
+        for det in pred:  # detections per image
+            im0 = im0s
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                
+        # return the results of detection (*xyxy, conf, cls)
+        return det
