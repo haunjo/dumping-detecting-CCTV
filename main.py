dbb0fda3b2450157adc874ae782b8af9239081bf
gnsdp99 <rladpgns99@gmail.com>
2023-04-14 11:50:52 +0900

    Update main code

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from modules.detect import Detector
 from modules.classify import Classifier
 from modules.crop import crop_image
 
-
-
 import torch
+import cv2
 #ImageFromVideo.get_image('data/video/2023032319.mov')
 
 # Initialize a class instance
@@ -17,20 +16,22 @@ human_detector = Detector()
 dumping_classifier = Classifier()
 
 start = time.time()
+cnt = 1
 path = "./data/images/2023_04_01_15"
 for img_path in os.listdir(path):
-    img_path = os.path.join(path, img_path)
+    src = os.path.join(path, img_path)
+    image = cv2.imread(src)
     with torch.no_grad():
-        det = human_detector.detect(img_path)
+        det = human_detector.detect(image)
 
     for i, (*xyxy, conf, cls) in enumerate(reversed(det)):
-        # xyxy -> (left_bottom_x, left_bottom_y, right_top_x, right_top_y)
+        # xyxy -> (left_top_x, left_top_y, right_bottom_x, right_bottom_y)
         if cls == 0: # only save an image of person
-            crropedImage = crop_image(img_path, tuple(map(float, xyxy)))
-            pred = dumping_classifier.classify(crropedImage)
-            print(pred)
-            if pred == "Dumping":
-                print(img_path)
+            crropedImage = crop_image(image, tuple(map(float, xyxy)))
+            pred, prob = dumping_classifier.classify(crropedImage)
+            if pred == "throwing away" and prob >= 76:
+                print(cnt, pred, prob, img_path, xyxy)
+                cnt += 1
                 # code to save the image in DB
         
 print(time.time() - start)
