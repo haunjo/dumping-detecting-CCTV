41010ef594100e44d28193af05586fd53aa0784a
haunJo <johaun12@naver.com>
2023-04-14 14:01:09 +0900

    api 수정

--- data/ImageFromVideo.py
+++ data/ImageFromVideo.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import base64
 from time import localtime
 from time import strftime
 
@@ -39,13 +40,14 @@ def get_image(path:str):
     
     while(video.isOpened()):
         ret, img = video.read()
-        if(int(video.get(1)) % int(fps/5) == 0): # get an image for each seconds
+        if(int(video.get(1)) % int(fps) == 0): # get an image for each seconds
             tm = localtime()
             capturedtime = strftime('%Y_%m_%d_%H%M%S', tm)
-            cv2.imwrite(f'data/images/{timestamp}/{capturedtime}{str(int(video.get(1)))}.jpg', img)
+            cv2.imwrite(f'images/{timestamp}/{capturedtime}{str(int(video.get(1)))}.jpg', img)
+            data = {"image": img, "filename" : f"{capturedtime}{str(int(video.get(1)))}"}
             print("Saved frame number:" , str(int(video.get(1))))
             count += 1
-            # yield f'data/images/{timestamp}/{capturedtime}.jpg' # yield path of image
+            yield data
         if(ret == False):
             break
     video.release()
--- data/ImageToAPI.py
+++ data/ImageToAPI.py
@@ -1,17 +1,27 @@
 import ImageFromCam
 import ImageFromVideo
+
 import requests
+import base64
+import cv2
+import json
+
+
 
 url =  "http://203.253.13.20:8000/files/"
-filename = "images/2023_04_12_191946.jpg"
 
-with open(filename, "rb") as f:
-    contents = f.read()
 
-files = {"file" : (filename, contents, "image/jpg")}
+def VideoToAPI():
+    for data in ImageFromVideo.get_image('test.mp4'):
+        _, img_encoded = cv2.imencode('.jpg', data["image"])
+        img_str = base64.b64encode(img_encoded).decode('utf-8')
+        request = {"image": img_str, "filename" : f'{data["filename"]}'}
+        response = requests.post(url, data=json.dumps(request))
+        print(response.json())
+def CamToAPI(): 
+    pass
 
-response = requests.post(url, files = files)
-print(response.json())
-
+if __name__=="__main__":
+    VideoToAPI()
 
 
