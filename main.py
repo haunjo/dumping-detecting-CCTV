65e8f7c92676a5526d793eccb5b0e080b5d3134d
gnsdp99 <rladpgns99@gmail.com>
2023-05-16 15:13:37 +0900

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import cv2
 import time
 import os
+import shutil
+path = "./data/images/2023_04_13_22/"
+
+import torch
 
 from data import ImageFromVideo
 from data import ImageFromCam
@@ -7,17 +12,18 @@ from modules.detect import Detector
 from modules.classify import Classifier
 from modules.crop import crop_image
 
-import torch
-import cv2
-#ImageFromVideo.get_image('data/video/2023032319.mov')
 
-# Initialize a class instance
+save_path = "./data/images/"
+
+total = 0
+
 human_detector = Detector()
 dumping_classifier = Classifier()
 
+print("Start")
+# for i in range(10):
 start = time.time()
 cnt = 1
-path = "./data/images/2023_04_01_15"
 for img_path in os.listdir(path):
     src = os.path.join(path, img_path)
     image = cv2.imread(src)
@@ -29,9 +35,23 @@ for img_path in os.listdir(path):
         if cls == 0: # only save an image of person
             crropedImage = crop_image(image, tuple(map(float, xyxy)))
             pred, prob = dumping_classifier.classify(crropedImage)
+#             if pred == "not throwing away":
             if pred == "throwing away" and prob >= 76:
+                dest = os.path.join(save_path, pred)
+                dest = os.path.join(dest, img_path)
+                try:
+                    shutil.copyfile(src, dest)
+                except FileExistsError as e:
+                    print(e)
+                # print(f"{img_path} is saved.")
                 print(cnt, pred, prob, img_path, xyxy)
+                
                 cnt += 1
+                break
                 # code to save the image in DB
-        
-print(time.time() - start)
+
+#     total += (time.time() - start)
+    
+# print(f"avg: {total/10}s")
+
+print("End")
