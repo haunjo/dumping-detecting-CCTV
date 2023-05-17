08ebf7eacfb38cbe8183b45fb587ed1c2fc0be43
= <johau12@naver.com>
2023-05-17 11:03:56 +0900

    S3 파일저장함수

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Request
 from fastapi.responses import JSONResponse
 import cv2
 import torch
@@ -20,31 +20,33 @@ import torch
 
 app = FastAPI()
 
+
 @app.post("/files/")
 async def create_file(file : Request):
     content = await file.json()
-    human_detector = Detector()
-    dumping_classifier = Classifier()
     img_data = base64.b64decode(content["image"])
     pil_image = Image.open(BytesIO(img_data))
-    img = cv2.imdecode(np.array(pil_image), cv2.COLOR_RGB2BGR)
+    cv_img = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
     with torch.no_grad():
-        det = human_detector.detect(img)
+        det = human_detector.detect(cv_img)
     for i, (*xyxy, conf, cls) in enumerate(reversed(det)):
         # xyxy -> (left_top_x, left_top_y, right_bottom_x, right_bottom_y)
         if cls == 0: # only save an image of person
-            crropedImage = crop_image(img, tuple(map(float, xyxy)))
+            crropedImage = crop_image(cv_img, tuple(map(float, xyxy)))
             pred, prob = dumping_classifier.classify(crropedImage)
             if pred == "throwing away" and prob >= 76:
-                print(cnt, pred, prob, xyxy)
-                cnt += 1
+                #print(cnt, pred, prob, xyxy)
+                return JSONResponse({"filename" : content["filename"], "action" : pred, "prob" : prob/100})
                 # code to save the image in DB
-    return JSONResponse({"filename" : content["filename"]})
+    return JSONResponse({"filename" : content["filename"], "action" : "None"})
 
 @app.get("/")
 async def root():
     return {"message" : "hello world"}
 
 if __name__ == "__main__":
+    human_detector = Detector()
+    dumping_classifier = Classifier()
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port = 8000)
+    
